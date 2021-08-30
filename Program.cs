9a9021363940f75eaaa72f4b0a0c7a53cc3dcf74
Kiera <kashiimaaa@gmail.com>
2021-08-30 07:24:05 +0200

    add pre release support by passing verison through the start-up arguements

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Minecraft_Wii_U_Mod_Injector_Updater {
-    static class Program {
+namespace Minecraft_Wii_U_Mod_Injector_Updater
+{
+    static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                MainForm.DownloadUrl =
+                    "https://github.com/Kashiiera/Minecraft-Wii-U-Mod-Injector/releases/latest/download/Minecraft.Wii.U.Mod.Injector.exe";
+            else
+                MainForm.DownloadUrl =
+                    "https://github.com/Kashiiera/Minecraft-Wii-U-Mod-Injector/releases/" + args[0] + "/download/Minecraft.Wii.U.Mod.Injector.exe";
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
